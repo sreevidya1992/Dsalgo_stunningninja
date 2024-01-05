76d08ded9a6943fa6605b56f94ba6db15a83705e
sreevidya1992 <153971293+sreevidya1992@users.noreply.github.com>
2024-01-05 18:54:29 -0500

    Update TestRunner.java

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.Cucumber;
 //@RunWith(Cucumber.class) //Junit execution
 
 	@CucumberOptions(
-			plugin = {"pretty","json:cucumber.json","html:target/MyReports/cucumber.html"}, //reporting purpose
+			plugin = {"pretty","json:target/cucumber/cucumber.json","html:target/MyReports/cucumber.html"}, //reporting purpose
 			monochrome=false,  //console output color
 			//tags = "@HomePage or @RegisterTS01 or @LoginTS02 or @DataStructuresTS03 or @stackFeature or @queueFeature or @Array or @LinkedList", //tags from feature file
 			features = {"src/test/resources/featurefiles"}, //location of feature files
